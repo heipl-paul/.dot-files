b8a9c2477ebf756e928c2b3402f8f324e4e2f35e
Paul Marian Heipl <heipl.paul@gmail.com>
2023-06-02 11:23:04 +0300

    no fix yet, just some cleanup and suggested modifications

--- zsh/.zsh-utils.sh
+++ zsh/.zsh-utils.sh
@@ -1,5 +1,26 @@
+#########################
+## Variable Definition ##
+#########################
+
+current_dir=$(dirname "$0")
+
 ############################################
 ## Util functions and additional nonsense ##
 ############################################
 
 export GPG_TTY=$(tty)                       # When I feel bold, replace $(tty) with $TTY and see if that works  # forces GPG geys to go through TTY for github
+
+##################
+###### PATH ######
+##################
+
+# append
+fpath+=("$current_dir/_gh")
+
+# export to sub-processes (make it inherited by child processes)
+export FPATH
+
+
+######## OTHER EXAMPLE ########
+# or prepend
+# path=('/your/path' $path)
